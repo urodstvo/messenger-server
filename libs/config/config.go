2d697c422e8ebe8a0eadd40fda1fe41b26b6125b
CLOWNAKIO <dead.kanji@mail.ru>
2024-06-27 14:45:03 +0300

    feat: added config loading from .env file

--- libs/config/config.go
+++ libs/config/config.go
@@ -1,20 +1,41 @@
 package config
 
-import "github.com/kelseyhightower/envconfig"
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/kelseyhightower/envconfig"
+
+	"github.com/joho/godotenv"
+)
 
 type Config struct {
 	EnvMode     string `envconfig:"ENVIRONMENT_MODE" default:"development"`
 	DatabaseUrl string `envconfig:"DATABASE_URL"`
 }
 
-func New() (*Config, error) {
+func NewWithEnvPath(envPath string) (*Config, error) {
 	var cfg Config
+	_ = godotenv.Load(envPath)
 
 	if err := envconfig.Process("", &cfg); err != nil {
 		return nil, err
 	}
 
 	return &cfg, nil
+}
+
+func New() (*Config, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	wd = filepath.Join(wd, "..", "..")
+
+	envPath := filepath.Join(wd, ".env")
+
+	return NewWithEnvPath(envPath)
 }
 
 func NewFx() Config {
--- libs/config/go.mod
+++ libs/config/go.mod
@@ -3,3 +3,5 @@ module github.com/urodstvo/messenger-server/libs/config
 go 1.22.4
 
 require github.com/kelseyhightower/envconfig v1.4.0
+
+require github.com/joho/godotenv v1.5.1
